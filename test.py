db1316115c16391b49f6a6d8bc367e7dd08bfc9c
Fsoky <cyberuest0x12@gmail.com>
2021-07-23 22:09:08 +0600

    Example

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
 from flask import Flask, redirect
-from donationalerts_api import DonationAlertsApi, Scopes
+from donationalerts_api import DonationAlertsApi, Scopes, Centrifugo
 
-client = Flask(__name__)
-api = DonationAlertsApi("client id", "client secret", "http://127.0.0.1/login", [Scopes.user_show, Scopes.donation_index])
+app = Flask(__name__)
+api = DonationAlertsApi("client id", "client secret", "http://127.0.0.1:5000/login", Scopes.all_scopes)
 
 
-@client.route("/", methods=["get"])
+@app.route("/", methods=["get"])
 def index():
-	return redirect(api.login())
+  return redirect(api.login())
+  
 
-
-@client.route("/login", methods=["get"])
+@app.route("/login", methods=["get"])
 def login():
-	code = api.get_code()
-	access_token = api.get_access_token(code)
+  """Beta ~"""
 
-	user = api.get_user(access_token)
-	return user
+  code = api.get_code()
 
+  access_token = api.get_access_token(code)
+  socket_token = api.get_user(access_token)["data"]["socket_connection_token"]
+  user_id = api.get_user(access_token)["data"]["id"]
+
+  fugo = Centrifugo(socket_token, access_token, user_id)
+  fugo.connect()
+
+  return fugo.subscribe()
 
 if __name__ == "__main__":
-	client.run(debug=True)
+  app.run(debug=True)
