3462b1b2caa99c6916c882c3a3e6d503dddd3067
Fsoky <cyberuest0x12@gmail.com>
2021-08-09 22:50:16 +0600

    Examples

--- test.py
+++ test.py
@@ -1,30 +1,33 @@
+# Centrifugo Test
+
 from flask import Flask, redirect
 from donationalerts_api import DonationAlertsApi, Centrifugo, Scopes, Channels
 
 app = Flask(__name__)
-api = DonationAlertsApi("client id", "client secret", "http://127.0.0.1:5000/login", Scopes.ALL_SCOPES)
+api = DonationAlertsApi("client id", "client secret", "http://127.0.0.1:5000/login",
+						[Scopes.USER_SHOW, Scopes.DONATION_SUBSCRIBE])
 
 
 @app.route("/", methods=["get"])
 def index():
-  return redirect(api.login())
-  
+	return redirect(api.login())
+
 
 @app.route("/login", methods=["get"])
 def login():
-  """Beta ~"""
+	code = api.get_code()
+	access_token = api.get_access_token(code)
 
-  code = api.get_code()
+	user = api.get_user(access_token)
+	socket_token = user["socket_connection_token"]
+	user_id = user["id"]
 
-  access_token = api.get_access_token(code)
-  socket_token = api.get_user(access_token)["data"]["socket_connection_token"]
-  user_id = api.get_user(access_token)["data"]["id"]
+	fugo = Centrifugo(socket_token, access_token, user_id)
+	fugo.connect()
+	fugo.subscribe(Channels.NEW_DONATION_ALERTS)
 
-  fugo = Centrifugo(socket_token, access_token, user_id)
-  fugo.connect()
-
-  return fugo.subscribe([Channels.NEW_DONATION_ALERTS])
-  
+	event = fugo.listen()
+	return event
 
 if __name__ == "__main__":
-  app.run(debug=True)
+	app.run(debug=True)
