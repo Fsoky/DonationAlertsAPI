0af241dbcb22bed1ccbbd7b54fe37ee56aabe624
Dan <55022836+Ykpauneu@users.noreply.github.com>
2023-03-29 18:24:32 +0300

    Flake8 fixes

--- donationalerts/constants.py
+++ donationalerts/constants.py
@@ -1,51 +1,57 @@
 class Scopes:
-	"""Обозначения
+    """Обозначения
 
-	USER_SHOW: получения данных профиля
+    USER_SHOW: получения данных профиля
 
-	DONATION_SUBSCRIBE: подписка на новые уведомления о пожертвованиях
-	DONATION_INDEX: просмотр пожертвований
+    DONATION_SUBSCRIBE: подписка на новые уведомления о пожертвованиях
+    DONATION_INDEX: просмотр пожертвований
 
-	CUSTOM_ALERT_STORE: создание пользовательских предупреждений
+    CUSTOM_ALERT_STORE: создание пользовательских предупреждений
 
-	GOAL_SUBSCRIBE: подписка на обновления целей пожертвований
-	POLL_SUBSCRIBE: подписка на обновления опросов
+    GOAL_SUBSCRIBE: подписка на обновления целей пожертвований
+    POLL_SUBSCRIBE: подписка на обновления опросов
 
-	ALL_SCOPES: все права в одном списке
+    ALL_SCOPES: все права в одном списке
 
-	"""
+    """
 
-	USER_SHOW = "oauth-user-show"
+    USER_SHOW = "oauth-user-show"
 
-	DONATION_SUBSCRIBE = "oauth-donation-subscribe"
-	DONATION_INDEX = "oauth-donation-index"
+    DONATION_SUBSCRIBE = "oauth-donation-subscribe"
+    DONATION_INDEX = "oauth-donation-index"
 
-	CUSTOM_ALERT_STORE = "oauth-custom_alert-store"
+    CUSTOM_ALERT_STORE = "oauth-custom_alert-store"
 
-	GOAL_SUBSCRIBE = "oauth-goal-subscribe"
-	POLL_SUBSCRIBE = "oauth-poll-subscribe"
+    GOAL_SUBSCRIBE = "oauth-goal-subscribe"
+    POLL_SUBSCRIBE = "oauth-poll-subscribe"
 
-	ALL_SCOPES = [USER_SHOW, DONATION_INDEX, DONATION_SUBSCRIBE, CUSTOM_ALERT_STORE,
-					GOAL_SUBSCRIBE, POLL_SUBSCRIBE]
+    ALL_SCOPES = [
+        USER_SHOW,
+        DONATION_INDEX,
+        DONATION_SUBSCRIBE,
+        CUSTOM_ALERT_STORE,
+        GOAL_SUBSCRIBE,
+        POLL_SUBSCRIBE,
+    ]
 
 
 class Channels:
-	"""Обозначения
+    """Обозначения
 
-	NEW_DONATION_ALERTS: новые оповещания о пожертвованиях
+    NEW_DONATION_ALERTS: новые оповещания о пожертвованиях
 
-	DONATION_GOALS_UPDATES: обновления целей пожертвования
+    DONATION_GOALS_UPDATES: обновления целей пожертвования
 
-	POLLS_UPDATES: обновления опросов
+    POLLS_UPDATES: обновления опросов
 
-	ALL_CHANNELS: все каналы в одном списке
+    ALL_CHANNELS: все каналы в одном списке
 
-	"""
+    """
 
-	NEW_DONATION_ALERTS = "$alerts:donation_"
+    NEW_DONATION_ALERTS = "$alerts:donation_"
 
-	DONATION_GOALS_UPDATES = "$goals:goal_"
+    DONATION_GOALS_UPDATES = "$goals:goal_"
 
-	POLLS_UPDATES = "$polls:poll_"
+    POLLS_UPDATES = "$polls:poll_"
 
-	ALL_CHANNELS = [NEW_DONATION_ALERTS, DONATION_GOALS_UPDATES, POLLS_UPDATES]
+    ALL_CHANNELS = [NEW_DONATION_ALERTS, DONATION_GOALS_UPDATES, POLLS_UPDATES]
